74d39f515a5fc1a46f9beeca0febb44eb2483816
hongguoguobaobei <hongguoguobaobei@gmail.com>
2014-07-17 10:29:59 +0800

    Use static middleware

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var express = require('express');
 var config = require('./configuration.json');
 
 var app = express();
+app.use(express.static(__dirname + '/public'));
 
 var server = http.createServer(app);
 server.listen(config['listen']['port'] || 80);
