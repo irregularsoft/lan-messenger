f794ed579d2ef71474240ac5f5f89b7d993852c2
hongguoguobaobei <hongguoguobaobei@gmail.com>
2014-07-17 11:48:53 +0800

    Use serve-favicon middleware

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var http = require('http');
 var express = require('express');
+var serveFavicon = require('serve-favicon');
 
 var config = require('./configuration.json');
 
 var app = express();
+app.use(serveFavicon(__dirname + '/public/favicon.ico'));
 app.use(express.static(__dirname + '/public'));
 
 var server = http.createServer(app);
