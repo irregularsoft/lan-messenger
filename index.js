094556a449a920640639eaf25226f67dcebd40b1
hongguoguobaobei <irregularsoft@gmail.com>
2014-07-17 16:04:46 +0800

    Configure ejs and convert sign-in.html to sign-in.ejs

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ var config = require('./configuration.json');
 var app = express();
 app.use(serveFavicon(__dirname + '/public/favicon.ico'));
 app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+var direct = require('./route/direct');
+app.get('/sign-in', direct({path: 'sign-in'}));
 
 var server = http.createServer(app);
 server.listen(config['listen']['port'] || 80);
