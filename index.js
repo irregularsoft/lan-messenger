a421654f0038d3a0f80da7010e2135e0bab63893
hongguoguobaobei <irregularsoft@gmail.com>
2014-07-17 16:45:01 +0800

    Install and use body-parser module

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var http = require('http');
 var express = require('express');
 var serveFavicon = require('serve-favicon');
+var bodyParser = require('body-parser');
 
 var config = require('./configuration.json');
 
 var app = express();
 app.use(serveFavicon(__dirname + '/public/favicon.ico'));
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 var direct = require('./route/direct');
