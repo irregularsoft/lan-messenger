bfa846cb89a6abd4cf1431fce9ed9eadebda6be9
hongguoguobaobei <irregularsoft@gmail.com>
2014-07-17 17:59:01 +0800

    Install and use cookie-parser, cookie-session module

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var http = require('http');
 var express = require('express');
 var serveFavicon = require('serve-favicon');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('cookie-session');
 
 var config = require('./configuration.json');
 
@@ -9,6 +11,11 @@ var app = express();
 app.use(serveFavicon(__dirname + '/public/favicon.ico'));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser('lan-messenger'));
+app.use(session({
+    'secret': 'lan-messenger',
+    'maxage': config['session']['maxage']
+}));
 app.set('view engine', 'ejs');
 
 var direct = require('./route/direct');
